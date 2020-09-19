4125e2cf0816f2bcefa213f0ad95bc609bce7251
Alan Smith <alan.smith@teamwork.com>
2020-09-19 22:12:13 +0100

    Dont build using vue-cli

--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,10 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Chart from '@/index';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(Chart, {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
